bd96bec8fa2826dfa80588ce1fc62718e452a66d
Flor77 <budaflorin@yahoo.com>
2023-02-10 21:23:15 +0200

    create sign in , sign up and forgot password pages and oauth comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [pageState, setPageState] = useState("Sign in");
+
   const location = useLocation();
   const navigate = useNavigate();
 
-  function pathMatchRoute(route) {
+  const pathMatchRoute = (route) => {
     if (route === location.pathname) {
       return true;
     }
-  }
+  };
 
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
             alt="logo"
             className="h-5 cursor-pointer"
-            onClick={() => {
-              navigate("/");
-            }}
+            onClick={() => navigate("/")}
           />
         </div>
         <div>
           <ul className="flex space-x-10">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/") && "text-black border-b-red-500"
+                pathMatchRoute("/") && "!text-black !border-b-red-500"
               }`}
-              onClick={() => {
-                navigate("/");
-              }}
+              onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/offers") && "text-black border-b-red-500"
+                pathMatchRoute("/offers") && "!text-black !border-b-red-500"
               }`}
-              onClick={() => {
-                navigate("/offers");
-              }}
+              onClick={() => navigate("/offers")}
             >
               Offers
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+                pathMatchRoute("/sign-in") && "!text-black !border-b-red-500"
               }`}
-              onClick={() => {
-                navigate("/sign-in");
-              }}
+              onClick={() => navigate("/sign-in")}
             >
-              Sign in
+              {pageState}
             </li>
           </ul>
         </div>
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Offers = () => {
-  return <div>Offers</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center mt-6 font-bold">Offers</h1>
+    </div>
+  );
 };
 
 export default Offers;
